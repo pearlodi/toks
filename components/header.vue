19d8b71bfc3cb6a3b815344572a0fe43015ee151
pearlodi7@gmail.com <you@example.com>
2024-05-14 08:05:17 +0100

    build header components footer components and table with functionalities

---
+++ components/header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex w-full px-8 py-3 items-center bg-gray-100">
+    <div class="w-12">
+      <img src="../assets/images/search-icon.png" alt="seacrh icon" class="w-full "/>
+    </div>
+    <div class="flex ml-6">
+      <div class="w-5">
+        <img src="../assets/images/save.png" alt="seacrh icon" class="w-full"/>
+      </div>
+      <div class="flex ml-4">
+        <div class="w-5">
+      <img src="../assets/images/arrow-left.png" alt="seacrh icon" class="w-full"/>
+        </div>
+        <div class="w-3 self-end justify-center">
+      <img src="../assets/images/down-arrow.png" alt="seacrh icon" class="w-full"/>
+        </div>
+      </div>
+
+      <div class="flex ml-4">
+        <div class="w-5">
+      <img src="../assets/images/arrow-right.png" alt="seacrh icon" class="w-full"/>
+        </div>
+        <div class="w-3 self-end justify-end">
+      <img src="../assets/images/down-arrow.png" alt="seacrh icon" class="w-full"/>
+        </div>
+      </div>
+    </div>
+    <div>
+      <ul class="flex ml-6">
+        <li class="text-[grey]">File</li>
+        <li class="ml-6  text-[grey]">Home</li>
+        <li class="ml-6  font-bold  border-b-[yellow]">Transform</li>
+        <li class="ml-6  text-[grey]">Add Column</li>
+        <li class="ml-6  text-[grey]">View</li>
+        <li class="ml-6  text-[grey]">Tools</li>
+        <li class="ml-6  text-[grey]">Help</li>
+      </ul>
+    </div>
+    <div class="ml-8">
+      <p class="font-bold">
+        Dataname - MyDataClinic Editor
+      </p>
+    </div>
+  </div>
+</template>
---
+++ components/tools.vue
@@ -0,0 +1,391 @@
+<template>
+  <div class="px-8 mt-4 pb-1 border-b-2 border-gray-200">
+    <div class="flex">
+      <div class="flex">
+        <div class="w-[100px]">
+          <div class="flex">
+            <div class="">
+              <p class="text-[gray] text-[8px]">Group by</p>
+            </div>
+            <div class="w-3 self-end justify-end">
+              <img
+                src="../assets/images/down-arrow.png"
+                alt="seacrh icon"
+                class="w-full ml-1"
+              />
+            </div>
+          </div>
+          <div class="w-[17px] mt-2">
+            <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+          </div>
+          <div class="flex mt-2">
+            <div class="flex items-center">
+              <div class="w-8">
+                <img
+                  src="../assets/images/copy.png"
+                  alt="copy"
+                  class="w-full"
+                />
+              </div>
+              <div>
+                <p class="text-[gray] text-[8px]">Use First Row as Headers</p>
+              </div>
+              <div class="w-4">
+                <img
+                  src="../assets/images/down-arrow.png"
+                  alt="seacrh icon"
+                  class="w-full"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="ml-4">
+          <div class="flex items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <p class="text-[gray] text-[8px]">Transpose</p>
+          </div>
+
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <p class="text-[gray] text-[8px]">Transpose</p>
+          </div>
+
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <p class="text-[gray] text-[8px]">Transpose</p>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+
+      <div class="ml-3 flex">
+        <div>
+          <div class="flex items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Data Type: Text</p>
+            </div>
+            <div class="w-4">
+              <img
+                src="../assets/images/down-arrow.png"
+                alt="seacrh icon"
+                class="w-full ml-1"
+              />
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Detect Data Type</p>
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Rename</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class="flex items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Data Type: Text</p>
+            </div>
+            <div class="w-4">
+              <img
+                src="../assets/images/down-arrow.png"
+                alt="seacrh icon"
+                class="w-full ml-1"
+              />
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Detect Data Type</p>
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Rename</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class="flex items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Data Type: Text</p>
+            </div>
+            <div class="w-4">
+              <img
+                src="../assets/images/down-arrow.png"
+                alt="seacrh icon"
+                class="w-full ml-1"
+              />
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Detect Data Type</p>
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Rename</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+
+      <div class="ml-3 flex items-center">
+        <div>
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class="flex items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Data Type: Text</p>
+            </div>
+            <div class="w-4">
+              <img
+                src="../assets/images/down-arrow.png"
+                alt="seacrh icon"
+                class="w-full ml-1"
+              />
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Detect Data Type</p>
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Rename</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+
+      <div class="ml-3 flex items-center">
+        <div>
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+      
+      <div class="ml-3 flex items-center">
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+
+      <div class="ml-3 flex items-center">
+        <div>
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class="flex items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Data Type: Text</p>
+            </div>
+            <div class="w-4">
+              <img
+                src="../assets/images/down-arrow.png"
+                alt="seacrh icon"
+                class="w-full ml-1"
+              />
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Detect Data Type</p>
+            </div>
+          </div>
+          <div class="flex items-center mt-2">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px]">Rename</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+
+      <div class="ml-3 flex items-center">
+        <div>
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-[#80808053] w-[1px] h-[60px]  ml-4"></div>
+
+      <div class="ml-3 flex items-center">
+        <div>
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+        <div class="ml-2">
+          <div class=" mt-2 w-8 flex flex-col justify-center items-center">
+            <div class="w-[17px]">
+              <img src="../assets/images/copy.png" alt="copy" class="w-full" />
+            </div>
+            <div>
+              <p class="text-[gray] text-[8px] text-center">Split Column</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
